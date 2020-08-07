d6f36b80404ade1b9f33ee2b5cb30b31816ff94d
midapsh <34818053+midapsh@users.noreply.github.com>
2020-08-07 02:02:10 -0300

    Add custom module CreateMuiTheme

--- src/themes/BaseTheme/index.tsx
+++ src/themes/BaseTheme/index.tsx
@@ -1,5 +1,43 @@
-import { createMuiTheme } from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core/styles';
 
-const BaseThemeProvider = createMuiTheme({});
+declare module '@material-ui/core/styles/createMuiTheme' {
+  interface Theme {
+    customPalette: {
+      common: {
+        arcBlue: string;
+        arcOrange: string;
+      };
+    };
+  }
+  // allow configuration using `createMuiTheme`
+  interface ThemeOptions {
+    customPalette: {
+      common: {
+        arcBlue: string;
+        arcOrange: string;
+      };
+    };
+  }
+}
+
+const arcBlue = '#0b72b9';
+const arcOrange = '#ffba60';
+
+const BaseThemeProvider = createMuiTheme({
+  palette: {
+    primary: {
+      main: arcBlue,
+    },
+    secondary: {
+      main: arcOrange,
+    },
+  },
+  customPalette: {
+    common: {
+      arcBlue,
+      arcOrange,
+    },
+  },
+});
 
 export default BaseThemeProvider;
