794b9ff275cf21c17e58d2b9c534cd215cfeca5d
midapsh <34818053+midapsh@users.noreply.github.com>
2020-08-10 12:14:24 -0300

    Create Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { useStyles } from './styles';
+
+const Footer: React.FC = () => {
+  return (
+    <>
+      <div>hello</div>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/styles';
+import {
+  createStyles,
+  Theme as AugmentedTheme,
+} from '@material-ui/core/styles';
+
+export const useStyles = makeStyles((theme: AugmentedTheme) => {
+  return createStyles({});
+});
