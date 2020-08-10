9dc29358d26f393bef1fdfa093c9eedae48c028a
midapsh <34818053+midapsh@users.noreply.github.com>
2020-08-10 13:15:03 -0300

    Add footer to homepage

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
+import footerAdornmentImage from '../../assets/Footer Adornment.svg';
+
+import { dummy } from './types';
 import { useStyles } from './styles';
 
 const Footer: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <>
-      <div>hello</div>
-    </>
+    <footer className={classes.footer}>
+      <img
+        className={classes.footerAdornment}
+        src={footerAdornmentImage}
+        alt="Black Decorative"
+      />
+    </footer>
   );
 };
 
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -5,5 +5,22 @@ import {
 } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme: AugmentedTheme) => {
-  return createStyles({});
+  return createStyles({
+    footer: {
+      backgroundColor: theme.customPalette.common.blue,
+      width: '100%',
+      zIndex: theme.zIndex.modal + 2,
+      position: 'relative',
+    },
+    footerAdornment: {
+      width: '20em',
+      verticalAlign: 'bottom',
+      [theme.breakpoints.down('md')]: {
+        width: '21em',
+      },
+      [theme.breakpoints.down('xs')]: {
+        width: '15em',
+      },
+    },
+  });
 });
