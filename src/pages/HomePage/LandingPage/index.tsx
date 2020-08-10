d3592462602f7f955b28d0b71dbb6d5a5426b8c0
midapsh <34818053+midapsh@users.noreply.github.com>
2020-08-10 18:24:48 -0300

    manage files

--- src/pages/HomePage/LandingPage/index.tsx
+++ src/pages/HomePage/LandingPage/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+
 import Lottie from 'react-lottie';
+import { Grid } from '@material-ui/core';
 
 import { defaultOptions } from './constants';
+import { useStyles } from './styles';
 
 const LandingPage: React.FC = () => {
-  return <Lottie options={defaultOptions} height={400} width={400} />;
+  const classes = useStyles();
+  return (
+    <Grid container direction="column">
+      <Grid item>
+        <Grid container direction="row">
+          <Grid item>
+            <summary>
+              Bringing West Coast Tecnology
+              <br />
+              to the Midwest
+            </summary>
+          </Grid>
+          <Grid item>
+            <Lottie options={defaultOptions} height={400} width={400} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default LandingPage;
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
 
-import { Grid } from '@material-ui/core';
-
 import LandingPage from './LandingPage';
 
-import { dummy } from './types';
-import { useStyles } from './styles';
-
 const HomePage: React.FC = () => {
-  const classes = useStyles();
   return (
-    <>
-      <div style={{ height: 685 }}>
-        <LandingPage />
-      </div>
-    </>
+    <div style={{ height: 685 }}>
+      <LandingPage />
+    </div>
   );
 };
 
